876fbd846552a80bf87d92b1826e00de6e05a16a
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-07-05 23:25:20 +0200

    act

--- server.js
+++ server.js
@@ -11,4 +11,22 @@ app.get("/quotes", function(request, response){
   response.json(quotes);
 });
 
+app.get("/quotes/:id", function(request, response){
+  let data = quotes;
+  let id = parseInt(request.params.id);
+  const resultQuote = data.find((quote)=>quote.id === id);
+  // resultado de filter si se sabe que solamente un elemento cumplia con la condicion filtro, y acceder luego al valor cero de esa nueva array resltandte 
+  response.send(resultQuote);
+});
+ 
+  // let data= quotes;
+  // let termId = parseInt(request.params.id);
+  // if (termId == number && termId <= 0 ) {
+  //   response.status(400).send("Number of ID not valid")
+  // } else {
+  //   let valueArray = data.filter( quote => quote.id === termId)
+  //   valueArray === [] ? response.status(404).send(` ${termId} Does not exist`) : response.status(200).json(valueArray);
+  // let selectQuote = quotes.filter( quote => quote.id === request);
+  // response.json(selectQuote);
+
 app.listen(3000, () => console.log("Listening on port 3000"));
